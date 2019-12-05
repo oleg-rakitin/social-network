04785f4d8cd4b6c9f66b22b6052a7adcaa0a95b9
Vlad Eremenko <vladsa123@mail.ru>
2019-12-05 11:22:49 +0300

    added jpa repository and message dto

---
+++ src/main/java/com/course/social/repos/UserRepo.java
@@ -0,0 +1,11 @@
+package com.course.social.repos;
+
+
+import com.course.social.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
