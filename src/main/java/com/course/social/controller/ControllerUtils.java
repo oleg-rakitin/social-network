cbf980d30b6a540e3e19d772f06907dca380154d
Artem Kuznetcov <artem.kuznetcov-cw@otsuka-us.com>
2020-04-04 04:37:55 +0300

    fix test coverage

--- src/main/java/com/course/social/controller/ControllerUtils.java
+++ src/main/java/com/course/social/controller/ControllerUtils.java
@@ -8,10 +8,10 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ControllerUtils {
+
     static Map<String, String> getErrors(BindingResult bindingResult) {
-        Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(
-                fieldError -> fieldError.getDefaultMessage() + "Error", FieldError::getDefaultMessage
-        );
+        Collector<FieldError, ?, Map<String, String>> collector = Collectors.toMap(fieldError -> fieldError.getDefaultMessage() + "Error", FieldError::getDefaultMessage);
         return bindingResult.getFieldErrors().stream().collect(collector);
     }
+
 }
